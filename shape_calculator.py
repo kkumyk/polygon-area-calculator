7a3bb6b8bf10a5cdc3e782c8793d6f1dee0390c2
kkumykova <38696352+kkumykova@users.noreply.github.com>
2022-02-07 18:08:09 +0000

    add set_height and set_width methods to subclass

--- shape_calculator.py
+++ shape_calculator.py
@@ -4,6 +4,9 @@ class Rectangle:
         self.width = width
         self.height = height
 
+    def __str__(self):
+        return "Rectangle(width=" + str(self.width) + ", height=" + str(self.height) + ")"
+
     def set_width(self, width):
         self.width = width
 
@@ -34,18 +37,25 @@ class Rectangle:
         max_height = self.height // other_shape.height
         return max_width * max_height
 
-    def __str__(self):
-        return "Rectangle(width=" + str(self.width) + ", height=" + str(self.height) + ")"
-
 
 class Square(Rectangle):
     def __init__(self, side_length):
-        self.width = side_length
-        self.height = side_length
+        super().__init__(side_length, side_length)
+
+    def __str__(self):
+        return "Square(side=" + str(self.width) + ")"
 
     def set_side(self, side_length):
         self.width = side_length
         self.height = side_length
 
-    def __str__(self):
-        return "Square(side=" + str(self.width) + ")"
+    def set_width(self, side_length):
+        self.width = side_length
+        self.height = side_length
+
+    def set_height(self, side_length):
+        self.width = side_length
+        self.height = side_length
+
+
+
